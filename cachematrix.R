935c47d0f14b7ff56de0ed3e5729566843f3113e
fortachong <darkforta@gmail.com>
2014-04-26 20:41:45 -0500

    Update cachematrix.R
    Coursera R Programming Assigment Week 3

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,9 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        ## Store a list in the global environment
+        ## Jorge Chong 
+        matrix_and_inverse <<- list(the_matrix = x, the_inverse = solve(x));  
 
 }
 
@@ -12,4 +15,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## Jorge Chong
+        ## First, test if there is a version of x cached
+        if(exists("matrix_and_inverse", globalenv()))
+        {
+                if(identical(x, matrix_and_inverse$the_matrix)) 
+                return(matrix_and_inverse$the_inverse)
+        }
+  
+        ## cache x and return its inverse
+        makeCacheMatrix(x)
+        matrix_and_inverse$the_inverse
 }
